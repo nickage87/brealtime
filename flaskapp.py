ded5f9ab740025f3c2c007c9ccb54ad33b66b60e
nickage87 <root@ip-172-31-42-91.us-east-2.compute.internal>
2017-09-20 01:49:25 +0000

    Added resume link

--- flaskapp.py
+++ flaskapp.py
@@ -3,6 +3,7 @@ from flask import request
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def webservice():
     if request.method == 'POST':
@@ -17,10 +18,21 @@ def webservice():
             return 'Yes, I can'
         elif request.args.get("q") == "Referrer":
             return 'Recruiter: Darren C.'
+        elif request.args.get("q") == "Position":
+            return 'Sr. Devops Engineer'
         elif request.args.get("q") == "Resume":
-            return "http://here/resume.docx"
+            return "http://52.15.70.14/files/resume.docx"
         elif request.args.get("q") == "Source":
             return "https://github.com/nickage87/brealtime.git"
-
+        elif request.args.get("q") == "Puzzle":
+            return "not solved yet source " + request.args.get("d")
+        elif request.args.get("q") == "Years":
+            return "I have experience of 6.5 years in Server Administration and DevOps"
+        elif request.args.get("q") == "Degree":
+            return "AS in Electronic Engineering & AS in Information Systems and Technologies"
+        elif request.args.get("q") == "Email Address":
+            return "madisona.mail@gmail.com"
+        elif request.args.get("q") == "Phone":
+            return "(347) 593-2765"
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0')
